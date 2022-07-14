2eda4ee67be9bea43c1b418095a2550e79bd8a6d
Gleb <gleb.prygunov2009@gmail.com>
2022-07-14 16:04:41 +0200

    Create win lines tab in js

--- project_js/BaseLogic.js
+++ project_js/BaseLogic.js
@@ -6,6 +6,7 @@ const symbolsNumberText = document.getElementById('symbolsNumber');
 const symbolsText = document.getElementById('symbol');
 const winLinesText = document.getElementById('winLinesText')
 const enterParametersButton = document.querySelector(`[class="enter"]`);
+const baseDiv = document.querySelector('.base');
 
 
 
@@ -25,13 +26,42 @@ enterParametersButton.addEventListener('click', (e) =>{
        allSymbols = symbolsText.value;
        allSymbols = winLinesText.value;
        divReels.remove();
-       console.log(amountReels, amountSymbols, allSymbols);
        div.classList.add('winLines');
-       document.querySelector('.base').append(div);
+       div.style.display = "table";
+       baseDiv.append(div);
+       for (let y = 0; y < amountSymbols; y++){
+           createNewDiv(`symbol${y}`, div, "block", false);
+           for (let x = 0; x < amountReels; x++){
+               createNewDiv(`x${x}y${y}`, `.symbol${y}`, "table", true, x, y);
+           }
+       }
+       //console.log(document.querySelector(".x3y1").y);
     } else {
         alert("Нou have an empty field!");
     }
 })
+
+function createNewDiv(divClassList, appendDiv, divDisplay, btn, x, y) {
+    let div = document.createElement('div');
+    div.classList.add(divClassList);
+    div.style.display = divDisplay;
+    div.style.height = "40px";
+    if (btn){
+        div.style.width = "40px";
+        div.style.backgroundColor = "yellow";
+        div.style.float = "left";
+        div.style.border = "1px solid darkblue"
+        div.style.x = x;
+        div.style.y = y;
+        document.querySelector(appendDiv).append(div);
+    }else{
+        appendDiv.append(div);
+        div.style.width = `${amountReels * 42}px`;
+        div.style.backgroundColor = "yellow";
+    }
+}
+
+
 
 
 
